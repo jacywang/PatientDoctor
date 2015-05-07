5153cf2fa34393fef66a9d8c76a3d439e181aeb2
Jacy Wang <jacywj@gmail.com>
2015-05-07 11:06:52 -0700

    Initialize repo

---
+++ PatientDoctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PatientDoctor
+//
+//  Created by JIAN WANG on 5/7/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
